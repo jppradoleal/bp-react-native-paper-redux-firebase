bcd54ca7f13ca93e380ce29c1c3164a23ed4c667
João Pedro Prado <joaopedro0128@hotmail.com>
2024-03-16 13:17:03 -0300

    Configures Firebase Auth
    Signed-off-by: João Pedro Prado <joaopedro0128@hotmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import { PaperProvider } from "react-native-paper";
 import { Provider as ReduxProvider } from 'react-redux'
 import { Routes } from "./routes";
 import { store } from "./store";
+import { MD3LightTheme } from 'react-native-paper'
 
 
 export function Main() {
   return <ReduxProvider store={store}>
-    <PaperProvider>
+    <PaperProvider theme={MD3LightTheme}>
       <Routes />
     </PaperProvider>
   </ReduxProvider>
